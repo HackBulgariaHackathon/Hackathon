38b4491a0be67213b471ee145db039bf575d010d
Sevgin Mustafov <sevgin.mustafov@outlook.com>
2015-05-26 22:44:21 +0300

    minor changes

--- pysuppl/docs/models.py
+++ pysuppl/docs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import Group, User
 
 # Create your models here.
+
+class Document(models.Model):
+    uploader = models.ForeignKey(User)
+    department = models.ForeignKey(Group)
+    name = models.CharField(max_length=255, null=False, blank=True)
+    file = models.FileField()
+    tags = models.TextField()
+    uploaded = models.DateTimeField()
