e6900c5918730c35718889be2a2e69948991078a
Sevgin Mustafov <sevgin.mustafov@outlook.com>
2015-05-26 14:21:57 +0300

    main url added

--- src/pysuppl/urls.py
+++ src/pysuppl/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import inventory.urls as inventory_urls
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^inventory/', include(inventory_urls)),
 ]
