2116fc8bc9018ca3554971ee77b09499d2b29c82
vassi95 <vassi__95@abv.bg>
2015-05-27 15:14:50 +0300

    start

--- vehicles/vehicle/admin.py
+++ vehicles/vehicle/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Vehicle, Paten_list
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    vehicle_display = ('status', 'reserved', 'returned')
+
+
+class Paten_listAdmin(admin.ModelAdmin):
+    list_display = ('name')
+
+admin.site.register(Vehicle, VehicleAdmin)
+admin.site.register(Paten_list, Paten_listAdmin)
