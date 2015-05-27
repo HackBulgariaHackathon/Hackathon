f9b46600a8af28531dbbbe29614aad65c9bc73e1
vassi95 <vassi__95@abv.bg>
2015-05-27 23:55:01 +0300

    .

---
+++ vehicles/vehicle/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+# Create your models here.
+
+
+class Vehicle(models.Model):
+    status = models.CharField(max_length=10, null=False, blank=False)
+    date_reserved = models.DateTimeField()
+    date_returned = models.DateTimeField()
+    user_id = models.ForeignKey(Paten_list)
+
+    def __str__(self):
+        return "{} - {} - {}".format
+        (self.status, self.date_reserved, self.date_returned)
+
+
+class Paten_list(models.Model):
+    user = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return "{}".format(self.user)
