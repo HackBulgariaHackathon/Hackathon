21c8e09009e8b63aad7f158f0121be83207b13df
presianbg <presianbg@gmail.com>
2015-05-28 11:27:43 +0300

    login OK -RC1

--- pysuppl/network_site/urls.py
+++ pysuppl/network_site/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
-from .views import index
+from .views import index, site_logout, site_login
 
 urlpatterns = [
-    url(r'^$', index),
+    url(r"^$", index, name="index"),
+    url(r"^logout/$", site_logout, name="logout"),
+    url(r"^login/$", site_login, name="login"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pysuppl/network_site/views.py
+++ pysuppl/network_site/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="/login")
 def index(request):
+    username = None
+    if request.user.is_authenticated():
+        username = request.user.username
+
+    return render(request, 'network_site/index.html', locals())
+
+
+def site_logout(request):
+    logout(request)
+    return redirect("network:index")
+
+
+def site_login(request):
+    if request.user.is_authenticated():
+        return redirect("network:index")
+
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
+        print (username, password)
         user = authenticate(username=username, password=password)
 
         if user is not None:
             login(request, user)
+            return redirect("network:index")
+        else:
+            return redirect("network:index")
 
-    return render(request, 'network_site/index.html', locals())
+    else:
+        return render(request, 'network_site/signin.html', locals())
