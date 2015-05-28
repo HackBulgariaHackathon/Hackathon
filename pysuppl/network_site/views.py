34e89f4adb63691cbbd3ed71265ffa28aa39ecb2
presianbg <presianbg@gmail.com>
2015-05-28 14:24:24 +0300

    added cooler Login() + error

--- pysuppl/network_site/views.py
+++ pysuppl/network_site/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 
 
@@ -20,18 +21,5 @@ def site_logout(request):
 def site_login(request):
     if request.user.is_authenticated():
         return redirect("network:index")
-
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        print (username, password)
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect("network:index")
-        else:
-            return redirect("network:index")
-
     else:
-        return render(request, 'network_site/signin.html', locals())
+        return auth_views.login(request, template_name='network_site/signin.html')
