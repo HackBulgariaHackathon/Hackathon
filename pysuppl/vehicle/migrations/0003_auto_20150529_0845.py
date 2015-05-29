c1c13d646dcbb80cc44b735e7129f36b175d75c1
Sevgin Mustafov <sevgin.mustafov@outlook.com>
2015-05-29 11:54:02 +0300

    Vehicles migrations added

---
+++ pysuppl/vehicle/migrations/0003_auto_20150529_0845.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('vehicle', '0002_vehicle_number'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='vehicle',
+            name='date_reserved',
+        ),
+        migrations.RemoveField(
+            model_name='vehicle',
+            name='date_returned',
+        ),
+        migrations.AlterField(
+            model_name='paten_list',
+            name='user',
+            field=models.CharField(blank=True, max_length=50),
+        ),
+        migrations.AlterField(
+            model_name='vehicle',
+            name='status',
+            field=models.CharField(max_length=11),
+        ),
+    ]
