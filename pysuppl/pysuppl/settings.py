379a4915389f72e4cc160a356d35fe0bc3e11390
Sevgin Mustafov <sevgin.mustafov@outlook.com>
2015-05-29 10:22:30 +0300

    Settings.py changed" ;

--- pysuppl/pysuppl/settings.py
+++ pysuppl/pysuppl/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 # STATIC_ROOT = '/home/sevgo/Projects/Hackathon/pysuppl/pusuppl/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATIC_URL = '/static/'
 STATIC_URL = '/static/'
