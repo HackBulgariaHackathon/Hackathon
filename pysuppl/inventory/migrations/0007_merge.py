1f70d73dcad45b5f203f33ea62e1e880e0a46efc
Sevgin Mustafov <sevgin.mustafov@outlook.com>
2015-05-29 12:18:09 +0300

    new migrations added

---
+++ pysuppl/inventory/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0002_auto_20150529_0907'),
+        ('inventory', '0006_remove_software_installed'),
+    ]
+
+    operations = [
+    ]
